cda16a2d3e724e77998768bbfca2f1e0dfa6ba43
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-11-12 11:27:02 +0100

    123

--- email.js
+++ email.js
@@ -1,24 +1,6 @@
-var nodemailer = require('nodemailer');
+var Mailchimp = require('mailchimp-api-v3');
+var mailchimp = new Mailchimp('e967d7216a9b36ab5e29f6359d10ed0-us5');
 
-var transporter = nodemailer.createTransport({
-  service: 'one',
-  auth: {
-    user: 'greenzebra@sainsh.com',
-    pass: 'Eepahwai1-'
-  }
-});
-
-var mailOptions = {
-  from: 'greenzebra@sainsh.com',
-  to: 'greenzebra@sainsh.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+mailchimp.get({path:'/lists/'},function (error,result) {
+  console.log(JSON.stringify(result))
+})
