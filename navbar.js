e8f7b55a82447d95db41d04298056cdb180181b3
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-11-12 13:26:58 +0100

    43234

--- navbar.js
+++ navbar.js
@@ -1,2 +1,2 @@
-fetch("C:/Users/Mads/Desktop/Web development/GreenZebra/navbar.html").then(res => res.text())
+fetch("navbar.html").then(res => res.text())
 .then((data) => document.getElementById("navbar").innerHTML = data);
