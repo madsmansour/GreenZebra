8942acc4295267be937fa4c179a02f49e734a02a
sainsh <kfsainsh@gmail.com>
2019-11-12 12:02:28 +0100

    changed stuff in email.js

--- email.js
+++ email.js
@@ -1,6 +1,12 @@
 var Mailchimp = require('mailchimp-api-v3');
-var mailchimp = new Mailchimp('e967d7216a9b36ab5e29f6359d10ed0-us5');
+var mailchimp = new Mailchimp('1e967d7216a9b36ab5e29f6359d10ed0-us5');
 
-mailchimp.get({path:'/lists/'},function (error,result) {
-  console.log(JSON.stringify(result))
+mailchimp.get({path:'/lists/3bb1070e53/segments/235833/members'},function (error,result) {
+  var emails = [];
+  for(var i = 0; i<result.members.length; i++){
+
+    emails[i] = result.members[i].email_address;
+  }
+  console.log(emails)
 })
+
